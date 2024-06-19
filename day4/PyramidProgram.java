34b39a7dd7853f7fb9602812fcee8a74aa205234
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-06-20 01:06:15 +0530

    feat: modified and uploaded

--- day4/PyramidProgram.java
+++ day4/PyramidProgram.java
@@ -2,15 +2,19 @@ package com.javacoding.day4;
 
 import java.util.Scanner;
 
-public class FloydsTriangle {
+public class PyramidProgram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rows: ");
         int rows = sc.nextInt();
         //outer-loop --> rows
         for(int i = 1; i <= rows; i++){
-            //inner loop --> columns
-            for(int j = 1; j <= i; j++){
+            //inner loop --> for spaces
+            for(int j = 1; j <= rows-i; j++){
+                System.out.print(" ");
+            }
+            //inner loop --> for stars
+            for(int j = 1; j <= 2*i-1; j++){
                 System.out.print("*");
             }
             System.out.println();
