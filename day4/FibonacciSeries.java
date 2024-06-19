55944b18b29261218fb0fe895867be004ab925f7
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-06-20 00:34:35 +0530

    feat: initial commit

---
+++ day4/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package com.javacoding.day4;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int limit = sc.nextInt();
+        int number1 = 1, number2 = 1, number3;
+
+        System.out.print(number1 + " ");
+        while (number2 <= limit) {
+            System.out.print(number2 + " ");
+            number3 = number1 + number2;
+            number1 = number2;
+            number2 = number3;
+
+        }
+    }
+}
