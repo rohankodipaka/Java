0a07916feb34db58f527cb3e32ca155cd07c4b58
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-06-21 00:25:58 +0530

    feat: uploaded LeapYear.java

---
+++ day4/LeapYear.java
@@ -0,0 +1,26 @@
+package com.javacoding.day4;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Year : ");
+        int year = sc.nextInt();
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.println(year + " is Leap Year");
+                } else{
+                    System.out.println(year + " is not a Leap Year");
+                }
+            } else{
+                System.out.println(year + " is Leap Year");
+            }
+        } else{
+            System.out.println(year + " is not a Leap Year");
+        }
+
+    }
+}
