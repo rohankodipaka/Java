3e113cb476d53978a73a3520819d242300edfce5
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-06-20 12:47:36 +0530

    feat: reverseArray.java uploaded

---
+++ day8/ReverseArray.java
@@ -0,0 +1,26 @@
+package com.javacoding.day8;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static int[] reverseAnArray(int[] numbers){
+        int i = 0;
+        int j = numbers.length - 1;
+        while(i<j){
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+            i++;
+            j--;
+        }
+        return numbers;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {2,3,1,17,4,5};
+        int[] reversed_numbers = reverseAnArray(numbers);
+        //for each loop
+        for(int number : reversed_numbers){
+            System.out.print(number + " ");
+        }
+    }
+}
