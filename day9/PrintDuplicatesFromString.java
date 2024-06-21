142ecbdc4eef9ac6401bb9e30f1272ef12d42bb1
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-06-21 13:18:03 +0530

    feat: uploaded PrintDuplicates.java

---
+++ day9/PrintDuplicatesFromString.java
@@ -0,0 +1,17 @@
+package com.javacoding.day9;
+
+
+
+public class PrintDuplicatesFromString {
+    public static void main(String[] args) {
+        String name = new String("idiodt");
+        char[] characters = name.toCharArray(); // name.toCharArray() converts name to char[].
+        for(int i = 0; i< characters.length; i++){
+            for(int j = i+1; j< characters.length; j++){
+                if(characters[i] == characters[j]){
+                    System.out.println("Duplicate Characters are : " + characters[j]);
+                }
+            }
+        }
+    }
+}
