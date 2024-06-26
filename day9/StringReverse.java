1e8a652611d601444cf4870c187f663d762a19e3
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-06-26 16:43:08 +0530

    feat: stringReverse.java uploaded

---
+++ day9/StringReverse.java
@@ -0,0 +1,17 @@
+package com.javacoding.day9;
+
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String name = sc.next();
+        char ch[] = name.toCharArray();
+        String reverse = "";
+        for(int i = ch.length-1; i>=0; i--){
+            reverse = reverse + ch[i];
+        }
+        System.out.println(reverse);
+    }
+}
