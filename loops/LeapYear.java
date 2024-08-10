0164553d391573fc3f4609a2b6876277e41a618c
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-08-10 17:55:00 +0530

    feat: Update LeapYear.java file

--- loops/LeapYear.java
+++ loops/LeapYear.java
@@ -8,19 +8,10 @@ public class LeapYear {
         System.out.println("Enter Year : ");
         int year = sc.nextInt();
 
-        if(year % 4 == 0){
-            if(year % 100 == 0){
-                if(year % 400 == 0){
-                    System.out.println(year + " is Leap Year");
-                } else{
-                    System.out.println(year + " is not a Leap Year");
-                }
-            } else{
-                System.out.println(year + " is Leap Year");
-            }
-        } else{
-            System.out.println(year + " is not a Leap Year");
-        }
+        if(year%4 ==0 && year%100!=0 || year%400==0)
+            System.out.println("leap year");
+        else
+            System.out.println("Not leap year");
 
     }
 }
