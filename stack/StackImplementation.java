672327917830d09694743b2a09392207a713be6e
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-08-20 12:28:23 +0530

    feat: Add StackImplementation.java file

---
+++ stack/StackImplementation.java
@@ -0,0 +1,41 @@
+package com.javacoding.stack;
+
+
+
+public class StackImplementation {
+    public static void main(String[] args) {
+
+        stack stack = new stack();
+
+        stack.push(3);
+        stack.push(6);
+        stack.push(7);
+        System.out.println("Top of the stack before deleting any element : " + stack.top());
+        System.out.println("Size of the stack before deleting any element : " + stack.size());
+        System.out.println("The element deleted is : " + stack.pop());
+        System.out.println("Size of the stack after deleting an element : " + stack.size());
+        System.out.println("Top of the stack after deleting an element : " + stack.top());
+    }
+}
+
+class stack{
+    int size = 10000;
+    int[] array = new int[size];
+    int top = -1;
+    void push(int item){
+        top++;
+        array[top] = item;
+    }
+    int pop(){
+        int removed = array[top];
+        top--;
+        return removed;
+    }
+    int top(){
+        return array[top];
+    }
+    int size(){
+        return top+1;
+    }
+
+}
