bbe5e1706605cb92f4847bf1f9a7077677531d84
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-08-26 19:32:34 +0530

    feat: Update LinkedList.java file

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -26,17 +26,47 @@ public class LinkedList {
         return head;
     }
 
+    private static int lengthOfLL(Node head){
+        int count = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next; // Move to the next node
+            count++;
+        }
+        return count;
+    }
+
+    private static boolean searchInLL(Node head, int value){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == value){
+                return true;
+            }
+            temp = temp.next; // Move to the next node
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         int[] values = {12,2,3,4,5};
         Node head = convertArrayToLL(values);
         // for finding head
-         System.out.println("Head is - " + head.data);
+         System.out.println("Head is : " + head.data);
 
         //Traverse through LL
         Node temp = head;
         while(temp != null){
             System.out.print(temp.data + " ");
-            temp = temp.next;
+            temp = temp.next; // Move to the next node
         }
+
+        System.out.println();
+
+        //Length of LL
+        System.out.println("Length of LL is : " + lengthOfLL(head));
+
+        //Search Element in LL
+        System.out.println(searchInLL(head, 15));
     }
 }
