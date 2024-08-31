a2c2c472a4b7474d2f3a1f00385db8bcb13354a5
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-08-31 10:21:15 +0530

    feat: Update StackImplementationUsingArrays.java file

--- stack/StackImplementationUsingArrays.java
+++ stack/StackImplementationUsingArrays.java
@@ -5,7 +5,7 @@ package com.javacoding.stack;
 public class StackImplementationUsingArrays {
     public static void main(String[] args) {
 
-        stack stack = new stack();
+        Stack stack = new Stack();
 
         stack.push(3);
         stack.push(6);
@@ -15,27 +15,52 @@ public class StackImplementationUsingArrays {
         System.out.println("The element deleted is : " + stack.pop());
         System.out.println("Size of the stack after deleting an element : " + stack.size());
         System.out.println("Top of the stack after deleting an element : " + stack.top());
+        stack.search(6);
     }
 }
 
-class stack{
-    int size = 10000;
-    int[] array = new int[size];
+class Stack{
+    int size = 5;
     int top = -1;
-    void push(int item){
-        top++;
-        array[top] = item;
+    int[] array = new int[size];
+
+    void push(int value){
+        if(top >= size){        //top >= 5
+            System.out.println("Stack is Full");
+        }
+        top = top+1;
+        array[top] = value;
     }
+
     int pop(){
-        int removed = array[top];
-        top--;
-        return removed;
+        int value = array[top];
+        top = top-1;
+        return value;
     }
+
     int top(){
         return array[top];
     }
+
     int size(){
         return top+1;
     }
+
+    void search(int value){
+        int index = -1;
+        for(int i=0;i<=top;i++){
+            if(value == array[i]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            System.out.println("Element not found");
+        }
+        else {
+            System.out.println("Element found at index : " + index);
+        }
+    }
+
 
 }
